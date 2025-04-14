73e44b5c6bc34e6f0ad875800d90514e2d193a8b
austinechisutia <austineakhonya624@gmail.com>
2025-04-14 20:01:03 +0300

    let gon on

---
+++ styles.cs
@@ -0,0 +1,29 @@
+@import url('https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&family=Roboto:ital,wght@0,100..900;1,100..900&family=Young+Serif&display=swap');
+
+*{
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+}
+:root{
+    --primary-color:rgb(249, 250, 249); /* Green */
+    --secondary-color: ; /* Red */
+    --background-color: #f5f5f5; /* Light Gray */
+    --text-color: #333; /* Dark Gray */
+    --font-family: 'Poppins', sans-serif;
+    --font-size: 16px;
+    --line-height: 1.6;
+}
+html{
+    scroll-behavior: smooth;
+
+}
+
+body{
+    font-family: 'Poppins', sans-serif;
+    background-color: #f5f5f5;
+    color: #333;
+    line-height: 1.6;
+    font-size: 16px;
+    overflow-x: hidden;
+}
